08156b299002ced374e9e523aa2754f561b80cad
Nick Fausti <ncfausti@gmail.com>
2023-03-01 22:58:54 -0500

    refactor to use only minutes.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // floor has passed, ceiling in future
-const t1 = new Date("2023-03-01T10:03:00.000Z"); // 10:03am
+const t1 = new Date("2023-03-01T10:00:00.000Z"); // 10:00am
 const t2 = new Date("2023-03-01T11:30:00.000Z"); // 11:30am
-const t3 = new Date("2023-03-01T14:32:00.000Z"); // 2:32pm
+const t3 = new Date("2023-03-01T14:30:00.000Z"); // 2:30pm
 
 // floor has passed, ceiling has passed
 const t4 = new Date("2023-03-01T10:00:00.000Z"); // 10:00am
@@ -33,6 +33,29 @@ const t19 = new Date("2023-03-01T10:45:00.000Z"); // 10:45am
 const t20 = new Date("2023-03-01T10:33:00.000Z"); // 10:33am
 const t21 = new Date("2023-03-01T10:55:00.000Z"); // 10:55am
 
+// floor in future, ceiling in future
+const t22 = new Date("2023-03-01T10:14:00.000Z"); // 10:14am
+const t23 = new Date("2023-03-01T10:33:00.000Z"); // 10:33am
+const t24 = new Date("2023-03-01T11:55:00.000Z"); // 11:55am
+
+function toFives(num) {
+  return Math.round(num / 5) * 5;
+}
+
+function fmtMinutes(minutes) {
+  if (minutes <= 0) return "";
+
+  if (minutes > 0 && minutes < 60) {
+    return `${toFives(minutes)}m`;
+  }
+
+  if (minutes >= 60 && minutes < 120) {
+    return `1h${toFives(minutes % 60)}m`;
+  }
+
+  return "2h";
+}
+
 function formatTime(floor, now, ceiling) {
   // get time differences for lower and upper bounds
   const timeToFloor = floor - now;
@@ -46,22 +69,13 @@ function formatTime(floor, now, ceiling) {
   // floor has passed, ceiling in future
   if (timeToFloor <= 0 && timeToCeiling > 0) {
     const minutes = timeToCeiling / 1000 / 60;
-    const hours = minutes >= 120 ? "2h" : minutes / 60;
-
-    if (hours === "2h") return hours;
-    if (hours === 1) return ["1h", toFive(minutes) + "m"].join("-");
-    return toFive(minutes) + "m";
+    return fmtMinutes(minutes);
   }
 
   // floor in future, ceiling in future
   if (timeToFloor > 0 && timeToCeiling > 0) {
-    const minutesCeiling = timeToCeiling / 1000 / 60;
-    const hoursCeiling =
-      minutesCeiling >= 120 ? "2h" : Math.floor(minutesCeiling / 60);
-
     const minutesFloor = timeToFloor / 1000 / 60;
-    const hoursFloor =
-      minutesFloor >= 120 ? "2h" : Math.floor(minutesFloor / 60);
+    const minutesCeiling = timeToCeiling / 1000 / 60;
 
     // Enumerate through all possible combinations:
 
@@ -69,35 +83,25 @@ function formatTime(floor, now, ceiling) {
     if (minutesFloor >= 120) return "2h";
 
     // minutesFloor 1hr+ ahead
-    if (minutesFloor >= 60 && hoursCeiling === "2h") {
-      return ["1h" + toFive(minutesFloor % 60) + "m", hoursCeiling].join("-");
+    if (minutesFloor >= 60 && minutesCeiling >= 120) {
+      return [fmtMinutes(minutesFloor), "2h"].join("-");
     }
 
-    // minutesFloor 1hr+ ahead and hoursCeiling === 1
-    if (minutesFloor >= 60 && hoursCeiling === 1) {
-      return [
-        "1h" + toFive(minutesFloor % 60) + "m",
-        hoursCeiling + "h" + toFive(minutesCeiling % 60) + "m",
-      ].join("-");
+    // minutesFloor 1hr+ ahead, minutesCeiling 1hr+ ahead
+    if (minutesFloor >= 60 && minutesCeiling >= 60) {
+      return [fmtMinutes(minutesFloor), fmtMinutes(minutesCeiling)].join("-");
     }
 
     // minutesFloor < 1hr ahead, hoursCeiling === 2h
-    if (minutesFloor < 60 && hoursCeiling === "2h") {
-      return [toFive(minutesFloor % 60) + "m", hoursCeiling].join("-");
+    if (minutesFloor < 60 && minutesCeiling > 120) {
+      return [fmtMinutes(minutesFloor), fmtMinutes(minutesCeiling)].join("-");
     }
 
     // minutesFloor < 1hr ahead, hoursCeiling < 1 hr ahead
-    if (minutesFloor < 60 && hoursCeiling === 0) {
-      return [
-        toFive(minutesFloor % 60) + "m",
-        toFive(minutesCeiling) + "m",
-      ].join("-");
+    if (minutesFloor < 60 && minutesCeiling < 60) {
+      return [fmtMinutes(minutesFloor), fmtMinutes(minutesCeiling)].join("-");
     }
   }
-}
-
-function toFive(num) {
-  return Math.round(num / 5) * 5;
 }
 
 const a = formatTime(t1, t2, t3);
@@ -107,6 +111,7 @@ const d = formatTime(t10, t11, t12);
 const e = formatTime(t13, t14, t15);
 const f = formatTime(t16, t17, t18);
 const g = formatTime(t19, t20, t21);
+const h = formatTime(t22, t23, t24);
 
 console.log(a);
 console.log(b);
@@ -115,3 +120,4 @@ console.log(d);
 console.log(e);
 console.log(f);
 console.log(g);
+console.log(h);
